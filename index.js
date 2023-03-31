cc824fa9d353fecbcae56ea70b261b6a4c676b39
Abdul Shakur Mohammed <abdshakurmohammed@gmail.com>
2023-03-31 21:19:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here 
+var multiply(num1,num2){
+    console.log(num1*num2)
+    num1=31;
+    num2=2
+    return (num1*num2)
+}
+var random(int){
+    console.log(int > 0)
+}
+var mod(num3,num4){num3 % num4
+return (4)}
+var max ()=20
